c5f0cce67e31255c1b11e4e9ae9d0d38443734f6
Jose Castillo <josegzcasz@gmail.com>
2022-06-12 23:34:13 -0500

    fix: bugs in the calculator and Names List components, with the property overflow: scroll and overflow: hidden, were fixed

--- src/App.js
+++ src/App.js
@@ -41,11 +41,105 @@ function App() {
 
   return (
     <div className="App">
-      <div className='logoContenedor'>
-        <img src={logoUTA} className='logoUta'
-        alt='logo de la Uta'/>
-      </div>
-      <div className='contenedorCalculadora'>
+
+        <div className='logoContenedor'>
+          <img src={logoUTA} className='logoUta'
+          alt='logo de la Uta'/>
+        </div>
+        <div className='inputBoton'>
+
+          <input
+                type="text"
+                name="name"
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+              />
+              <input
+                type="text"
+                name="lastName"
+                onChange={(event) => {
+                  setLastname(event.target.value);
+                }}
+              />
+            
+            <button className='agregarNombre'
+              onClick={() => {
+                setPeople((current) => [{ name, lastName }, ...current]);
+              }}
+            >
+              Add 
+            </button>
+        </div>
+       <div className='contenedorGeneral'>
+      <div className='izquierda'>
+
+      <div className="">
+        
+          <div className='inputBoton'>
+         
+
+
+
+
+
+
+
+
+
+          
+        <div className='contenedorTabla'> 
+           
+            <div className='tituloLista'>Nombre</div>
+            <div className='lista'>
+              {people.map((person, idx) => (
+                  <li className='listaNombre' id={idx}>{`${person.name} ${person.lastName}`}</li>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+      {/* <div className='contenedorCalculadora'>
+        <Pantalla input={input}/>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>     
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={resultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonDelete Del ={() => setInput('')}>
+          Delete
+          </BotonDelete>
+        </div>
+      </div> */}
+    </div>
+      <div className='wrapper'>
+        
+      
+
+    <div className='derecha'> 
+
+    <div className='contenedorCalculadora'>
         <Pantalla input={input}/>
         <div className='fila'>
           <Boton manejarClic={agregarInput}>7</Boton>
@@ -77,38 +171,47 @@ function App() {
           </BotonDelete>
         </div>
       </div>
-  
-    <div className="tableContainer">
-      <div>
-        <input
-          type="text"
-          name="name"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          name="lastName"
-          onChange={(event) => {
-            setLastname(event.target.value);
-          }}
-        />
-        <button className='agregarNombre'
-          onClick={() => {
-            setPeople((current) => [{ name, lastName }, ...current]);
-          }}
-        >
-          Add user
-        </button>
-        <th>
-          <ul>
-            {people.map((person, idx) => (
-              <li id={idx}>{`${person.name} ${person.lastName}`}</li>
-            ))}
-          </ul>
-         </th>
+
+      <div className="">
+        
+          <div className='inputBoton'>
+         
+            {/* <input
+              type="text"
+              name="name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
+            <input
+              type="text"
+              name="lastName"
+              onChange={(event) => {
+                setLastname(event.target.value);
+              }}
+            />
+
+          <button className='agregarNombre'
+            onClick={() => {
+              setPeople((current) => [{ name, lastName }, ...current]);
+            }}
+          >
+            Add 
+          </button> */}
+          
+        {/* <div className='contenedorTabla'> 
+           
+            <div className='tituloLista'>Nombre</div>
+            <div className='lista'>
+              {people.map((person, idx) => (
+                  <li className='listaNombre' id={idx}>{`${person.name} ${person.lastName}`}</li>
+                ))}
+              </div>
+            </div> */}
+          </div>
         </div>
+      </div>
+      </div>
       </div>
     </div>
   );
