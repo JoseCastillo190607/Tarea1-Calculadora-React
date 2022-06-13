5ad88ef06e3b76e34ae39fabcba2bf1b350473a5
Jose Castillo <josegzcasz@gmail.com>
2022-06-12 23:40:35 -0500

    style: the code was formatted to improve readability

--- src/App.js
+++ src/App.js
@@ -62,7 +62,6 @@ function App() {
                   setLastname(event.target.value);
                 }}
               />
-            
             <button className='agregarNombre'
               onClick={() => {
                 setPeople((current) => [{ name, lastName }, ...current]);
@@ -73,21 +72,8 @@ function App() {
         </div>
        <div className='contenedorGeneral'>
       <div className='izquierda'>
-
       <div className="">
-        
-          <div className='inputBoton'>
-         
-
-
-
-
-
-
-
-
-
-          
+        <div className='inputBoton'>
         <div className='contenedorTabla'> 
            
             <div className='tituloLista'>Nombre</div>
@@ -99,44 +85,7 @@ function App() {
             </div>
           </div>
         </div>
-
-      {/* <div className='contenedorCalculadora'>
-        <Pantalla input={input}/>
-        <div className='fila'>
-          <Boton manejarClic={agregarInput}>7</Boton>
-          <Boton manejarClic={agregarInput}>8</Boton>
-          <Boton manejarClic={agregarInput}>9</Boton>
-          <Boton manejarClic={agregarInput}>*</Boton>
-        </div>
-        <div className='fila'>
-          <Boton manejarClic={agregarInput}>4</Boton>
-          <Boton manejarClic={agregarInput}>5</Boton>
-          <Boton manejarClic={agregarInput}>6</Boton>
-          <Boton manejarClic={agregarInput}>-</Boton>
-        </div>
-        <div className='fila'>
-          <Boton manejarClic={agregarInput}>1</Boton>
-          <Boton manejarClic={agregarInput}>2</Boton>
-          <Boton manejarClic={agregarInput}>3</Boton>
-          <Boton manejarClic={agregarInput}>+</Boton>
-        </div>     
-        <div className='fila'>
-          <Boton manejarClic={agregarInput}>0</Boton>
-          <Boton manejarClic={agregarInput}>.</Boton>
-          <Boton manejarClic={resultado}>=</Boton>
-          <Boton manejarClic={agregarInput}>/</Boton>
-        </div>
-        <div className='fila'>
-          <BotonDelete Del ={() => setInput('')}>
-          Delete
-          </BotonDelete>
-        </div>
-      </div> */}
     </div>
-      <div className='wrapper'>
-        
-      
-
     <div className='derecha'> 
 
     <div className='contenedorCalculadora'>
@@ -173,44 +122,7 @@ function App() {
       </div>
 
       <div className="">
-        
-          <div className='inputBoton'>
-         
-            {/* <input
-              type="text"
-              name="name"
-              onChange={(event) => {
-                setName(event.target.value);
-              }}
-            />
-            <input
-              type="text"
-              name="lastName"
-              onChange={(event) => {
-                setLastname(event.target.value);
-              }}
-            />
-
-          <button className='agregarNombre'
-            onClick={() => {
-              setPeople((current) => [{ name, lastName }, ...current]);
-            }}
-          >
-            Add 
-          </button> */}
-          
-        {/* <div className='contenedorTabla'> 
-           
-            <div className='tituloLista'>Nombre</div>
-            <div className='lista'>
-              {people.map((person, idx) => (
-                  <li className='listaNombre' id={idx}>{`${person.name} ${person.lastName}`}</li>
-                ))}
-              </div>
-            </div> */}
-          </div>
         </div>
-      </div>
       </div>
       </div>
     </div>
